22a7d1e0474cdf2fb12d57ae435f3cc9a01bd9a1
Quenproject1 <116909252+Quenproject1@users.noreply.github.com>
2022-10-29 06:41:34 +0700

    Add files via upload

---
+++ quen2.py
@@ -0,0 +1,45 @@
+#coding: utf-8
+# module
+import os,sys,time,getpass
+
+# username
+x = "QUEN PROJECT"
+# password
+y = "QUEN"
+
+
+# mengetik otomatis cepat
+def sp(a):
+  for e in a + "\n":
+    sys.stdout.write(e)
+    sys.stdout.flush()
+    time.sleep(0.002)
+
+
+# login
+def login():
+  os.system("clear")
+  sp("\033[1;96m                                                                   ") 
+  sp("┏━━━┳┓╋┏┳━━━┳━┓╋┏┓┏━━━┳━━━┳━━━┓╋┏┳━━━┳━━━┳━━━━┓   ") 
+  sp("┃┏━┓┃┃╋┃┃┏━━┫┃┗┓┃┃┃┏━┓┃┏━┓┃┏━┓┃╋┃┃┏━━┫┏━┓┃┏┓┏┓┃   ") 
+  sp("┃┃╋┃┃┃╋┃┃┗━━┫┏┓┗┛┃┃┗━┛┃┗━┛┃┃╋┃┃╋┃┃┗━━┫┃╋┗┻┛┃┃┗┛   ") 
+  sp("┃┃╋┃┃┃╋┃┃┏━━┫┃┗┓┃┃┃┏━━┫┏┓┏┫┃╋┃┣┓┃┃┏━━┫┃╋┏┓╋┃┃      ") 
+  sp("┃┗━┛┃┗━┛┃┗━━┫┃╋┃┃┃┃┃╋╋┃┃┃┗┫┗━┛┃┗┛┃┗━━┫┗━┛┃╋┃┃      ") 
+  sp("┗━━┓┣━━━┻━━━┻┛╋┗━┛┗┛╋╋┗┛┗━┻━━━┻━━┻━━━┻━━━┛╋┗┛      ") 
+  sp("╋╋╋┗┛                                                                      ") 
+  sp("\033[1;96m+\033[1;90m========================================\033[1;96m+")
+  sp(" \033[1;97m{\033[1;95m•\033[1;97m} \033[1;93mAuthor   \033[1;91m: \033[1;95mQUEN PROJECT")
+  sp(" \033[1;97m{\033[1;95m•\033[1;97m} \033[1;93mFacebook \033[1;91m: \033[1;95mQUEN PROJECT1")
+  sp(" \033[1;97m{\033[1;95m•\033[1;97m} \033[1;93mYouTube  \033[1;91m: \033[1;95mQUEN PROJECT")
+  sp(" \033[1;97m{\033[1;95m•\033[1;97m} \033[1;93mGithub   \033[1;91m: \033[4;92mhttps://github.com/Quenproject1\033[1;0m")
+  sp("\033[1;96m+\033[1;90m========================================\033[1;96m+")
+
+  if username == x and password == y:
+    print(" \033[1;92mAccess Grented")
+    time.sleep(1)
+  else:
+    print(" \033[1;91mAccess Denied")
+    time.sleep(1)
+    login()
+
+login()
